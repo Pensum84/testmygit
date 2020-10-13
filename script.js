778af5e9df37f75efe6282ba447df1ca8da0e3a8
Cilla <57146695+Pensum84@users.noreply.github.com>
2020-10-13 10:38:54 +0200

    Ändringar inför inlämningen av individuella 1

--- script.js
+++ script.js
@@ -1,39 +1,35 @@
-function myFunction(){
-var person = alert("Fyll i formuläret så återkommer jag så snart som möjligt");
-}
-
-var form = document.getElementById("form")
-form.addEventListener("submit", ()=> {
-    validationForm()
-})
 
 function validationForm(){
     var fname = document.getElementById("fname").value;
     var lname = document.getElementById("lname").value;
-    var message = document.getElementById("message").value;
     var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var text;
 
+    error_message.style.padding = "10px";
 
     if(fname.length <1){
-        alert("Please enter a Name");
-        
+        text = "Var god skriv in ett giltigt förnamn";
+        error_message.innerHTML = text;
         return false;
     }
     else if(lname.length <1){
-        alert("Please enter a Name");
-
+        text = "Var god skriv in ett giltigt efternamn";
+        error_message.innerHTML = text;
         return false;
     }
-    else if((document.kontakt.email.value.indexOf('@') == -1) ||
-    (document.kontakt.email.value.indexOf('.') == -1)) {
-        alert("Please enter a valid Email");
-
+    if(email.indexOf("@") == -1 || email.length <5){
+        text = "Var god ange en riktig email adress";
+        error_message.innerHTML = text;
         return false;
     }
-
-    else if(document.kontakt.message.length <10){
-        alert("Please enter a message before submitting.");
-
+    if(message.length <10){
+        text = "Glöm inte att skriva in ett meddelande";
+        error_message.innerHTML = text;
         return false;
     }
+    alert("Formuläret skickades utan problem vi återkommer inom kort");
+
+    return false;
+
 }
